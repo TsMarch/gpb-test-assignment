3c442be5edc0648158eb0bbc55dcbb05c68d6f2e
Alexey Zhuravlev <81296855+TsMarch@users.noreply.github.com>
2024-05-17 11:21:06 +0500

    added task6

---
+++ task6/dag.py
@@ -0,0 +1,28 @@
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+import datetime
+from airflow.providers.http.hooks.http import HttpHook
+
+kwargs = {
+    'owner': 'airflow',
+    'start_date': datetime.datetime(2024, 5, 17),
+    'retries': 1,
+    'retry_delay': datetime.timedelta(minutes=1),
+    'depends_on_past': False,
+}
+
+def hook_function(**kwargs):
+    start_date = kwargs["start_date"]
+    api_url = f"127.0.0.1:7500/api.gazprombank.ru/very/important/docs/{start_date}"
+    hook = HttpHook(method="GET", http_conn_id=api_url)
+    response = hook.run(endpoint='', headers={}, extra_options={'data': {'url': api_url}})
+    return response
+
+
+with DAG(dag_id='download_json', default_args=kwargs, schedule_interval=None) as dag:
+    download_json = PythonOperator(
+        task_id='download_api_json',
+        python_callable=hook_function,
+        dag=dag
+    )
+download_json
